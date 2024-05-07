2f5c2849df209f5849cd72773c90358511982592
Tamás Tóth <tomjtoth@gmail.com>
2024-05-07 13:33:46 +0300

    implemented `/health` -> `HTTP 200` endpoint

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (_req, res) => {
+  res.status(200).end()
+})
+
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
