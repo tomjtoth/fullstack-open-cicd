5ecbd934506bf92d0898e7985a356b33f1d39e54
Tamás Tóth <tomjtoth@gmail.com>
2024-05-07 13:41:04 +0300

    simulate broken healthcheck

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error...')
   res.status(200).end()
 })
 
